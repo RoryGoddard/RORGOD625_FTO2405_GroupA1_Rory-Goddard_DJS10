69e6b8e4202f96bc1fb1ed1b70f880b29f00816f
Rory Goddard <goddardrory@gmail.com>
2024-10-07 16:21:29 +0200

    Feat: Example async await fetch request inside useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState, UseEffect } from "react";
+import React from "react";
 import './App.css'
 
-Const URL = "https://jsonplaceholder.typicode.com/posts"
+const URL = "https://jsonplaceholder.typicode.com/posts"
 
 function App() {
+
+  const [blogData, setBlogData] = React.useState([{body: "I really hope this works"}])
+  
+  React.useEffect(() => {
+    const fetchBlogPosts = async () => {
+      const result = await fetch(URL)
+      result.json()
+      .then(result => {
+        setBlogData(result)
+      }).catch((error) => {
+        console.log(`Something went wrong. Error: ${error}`)
+      })
+    }
+    fetchBlogPosts()
+  }, [])
+  
 
   return (
     <>
       <h1>DJS10 - Asynchrony</h1>
+      <p>{blogData[0].body}</p>
     </>
   )
 }
